23f8869d0fb53a51ee745377d1bd5c95843e6f87
Ivan Yip <ivan_yip@msn.com>
2018-01-08 18:47:25 +0800

    working on backend api

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const reload = require('reload');
+const watch = require('watch');
+const bodyParser = require("body-parser");
+const jwt = require('jwt-simple');
+const axios = require('axios');
+
+const api = require('./router-api');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Reloading the backend when backend is changed.
+reloadServer = reload(app);
+
+watch.watchTree(__dirname + "/frontend/dist", function (f, curr, prev) {
+  // Fire server-side reload event 
+  reloadServer.reload();
+});
+
+app.use(express.static('frontend/dist'));
+
+app.use('/api', api);
+
+app.use((req, res, next) => {
+  res.sendFile(__dirname + "/frontend/dist/index.html");
+});
+
+app.listen(8080);
---
+++ router-api.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const redis = require('redis');
+const client = redis.createClient({
+  host: 'localhost',
+  port: 6379
+});
+
+client.on('error', (err) => {
+  console.log(err);
+});
+
+const groups = [
+  {
+    id: 0,
+    name: "Group A"
+  },
+  {
+    id: 1,
+    name: "Group B"
+  },
+  {
+    id: 2,
+    name: "Group C"
+  }
+]
+
+const users = [
+  {
+    id: 0,
+    name: "John Doe",
+    email: "john.doe@gmail.com",
+    password: "123456"
+  },
+  {
+    id: 1,
+    name: "Chan Siu Ming",
+    email: "chan.siu.ming@gmail.com",
+    password: "123456"
+  },
+  {
+    id: 2,
+    name: "Juan Nadie",
+    email: "juan.nadie@gmail.com",
+    password: "123456"
+  }
+]
+
+router.get('/groups', auth.authenticate(), (req, res) => {
+  res.json(groups);
+});
+
+router.get('/users', auth.authenticate(), (req, res) => {
+  res.json(users);
+});
+
+router.get('/links'), (req, res) => {
+  client.get('links', function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    res.json(data);
+  });
+}
+
+router.post('/link'), (req, res) => {
+  let link = {
+    title: req.body.title,
+    url: req.body.url,
+    tags: req.body.tags
+  }
+  let links;
+
+  if (client.get('links') === null) {
+    links = [];
+  } else {
+    client.get('location', function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      links = JSON.parse(data);
+      // if (err) {
+      //   return console.log(err);
+      // }
+      // console.log('The value is ', data);
+    });
+  }
+  links.unshift(link);
+  client.set('links', JSON.stringify(links), (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+}
+
+module.exports = router;
